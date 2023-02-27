2044c8f7da9a09b1ce625b1252e599e39e22931f
x.karimjonov <khtkarimzhonov@gmail.com>
2023-02-27 11:19:05 +0500

    first commit -m

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.9
+
+RUN apt-get update \
+  && apt-get -y install tesseract-ocr \
+  && apt-get install -y python3 python3-distutils python3-pip 
+
+RUN apt update \
+  && apt-get install ffmpeg libsm6 libxext6 -y
+RUN pip3 install pytesseract
+RUN pip3 install opencv-python
+
+COPY . /app
+WORKDIR /app
+
+RUN pip install imutils pandas
+
+CMD ["python", "test.py"]
--- test.py
+++ test.py
@@ -1,59 +1,53 @@
-import numpy as np
 import cv2
-import  imutils
-import sys
 import pytesseract
-import pandas as pd
-import time
 
-image = cv2.imread('car.jpeg')
+carplate_img = cv2.imread('images/skode.jpg')
+carplate_img_rgb = cv2.cvtColor(carplate_img, cv2.COLOR_BGR2RGB)
 
-image = imutils.resize(image, width=500)
+carplate_haar_cascade = cv2.CascadeClassifier('plate.xml')
 
-cv2.imshow("Original Image", image)
+def carplate_detect(image):
+    carplate_overlay = image.copy() # Create overlay to display red rectangle of detected car plate
+    carplate_rects = carplate_haar_cascade.detectMultiScale(carplate_overlay,scaleFactor=1.1, minNeighbors=5) 
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#cv2.imshow("1 - Grayscale Conversion", gray)
+    for x,y,w,h in carplate_rects: 
+        cv2.rectangle(carplate_overlay, (x,y), (x+w,y+h), (255,0,0), 5) 
+        
+    return carplate_overlay
 
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-#cv2.imshow("2 - Bilateral Filter", gray)
+detected_carplate_img = carplate_detect(carplate_img_rgb)
+# enlarge_plt_display(detected_carplate_img, 1.2)
 
-edged = cv2.Canny(gray, 170, 200)
-#cv2.imshow("4 - Canny Edges", edged)
+def carplate_extract(image):
+    
+    carplate_rects = carplate_haar_cascade.detectMultiScale(image,scaleFactor=1.1, minNeighbors=5) 
 
-(new, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:30] 
-NumberPlateCnt = None 
+    for x,y,w,h in carplate_rects: 
+        carplate_img = image[y+15:y+h-10 ,x+15:x+w-20] 
+        
+    return carplate_img
 
-count = 0
-for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        if len(approx) == 4:  
-            NumberPlateCnt = approx 
-            break
 
-# Masking the part other than the number plate
-mask = np.zeros(gray.shape,np.uint8)
-new_image = cv2.drawContours(mask,[NumberPlateCnt],0,255,-1)
-new_image = cv2.bitwise_and(image,image,mask=mask)
-cv2.namedWindow("Final_image",cv2.WINDOW_NORMAL)
-cv2.imshow("Final_image",new_image)
+def enlarge_img(image, scale_percent):
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    return resized_image
 
-# Configuration for tesseract
-config = ('-l eng --oem 1 --psm 3')
+carplate_extract_img = carplate_extract(carplate_img_rgb)
+carplate_extract_img = enlarge_img(carplate_extract_img, 150)
+# plt.imshow(carplate_extract_img);
 
-# Run tesseract OCR on image
-text = pytesseract.image_to_string(new_image, config=config)
+carplate_extract_img_gray = cv2.cvtColor(carplate_extract_img, cv2.COLOR_RGB2GRAY)
+# plt.axis('off') 
+# plt.imshow(carplate_extract_img_gray, cmap = 'gray');
 
-#Data is stored in CSV file
-raw_data = {'date': [time.asctime( time.localtime(time.time()) )], 
-        'v_number': [text]}
+carplate_extract_img_gray_blur = cv2.medianBlur(carplate_extract_img_gray,3) # Kernel size 3
+# plt.axis('off') 
+# plt.imshow(carplate_extract_img_gray_blur, cmap = 'gray');
 
-df = pd.DataFrame(raw_data, columns = ['date', 'v_number'])
-df.to_csv('data.csv')
-
-# Print recognized text
-print(text)
-
-cv2.waitKey(0)
+print(pytesseract.image_to_string(
+    carplate_extract_img_gray_blur, 
+    config = f'--psm 8 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+)
