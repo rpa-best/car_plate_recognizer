44ee3d36245c41411c17935c4b8a5624ef6a45c2
Khusniddin <khtkarimzhonov@gmail.com>
2023-05-22 21:06:04 +0500

    test

--- services.py
+++ services.py
@@ -7,6 +7,6 @@ class CarControlService:
     def plate_response(self, plate: list, camera_params: dict):
         for p in plate:
             camera_params.update(plate=p)
-            self.header.update({"Idempotency-Key": uuid.uuid4()})
+            self.header.update({"Idempotency-Key": str(uuid.uuid4())})
             response = requests.post(camera_params.get('plate_response_url'), json=camera_params, headers=self.header)
             print(response.json())
